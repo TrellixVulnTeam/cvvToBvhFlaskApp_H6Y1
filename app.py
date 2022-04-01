3dba978676666f861af2d1c6fa4444af57573163
Rahul <90084850+grahul27@users.noreply.github.com>
2022-03-31 20:10:09 -0600

    working POST api to upload file

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
-from flask import Flask
+import os
+import urllib.request
+from flask import Flask, request, redirect, jsonify,render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
 
 UPLOAD_FOLDER = 'file_uploads'
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='./flaskApp/template')
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','csv'])
+
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+@cross_origin()
+def uploadForm():
+    return render_template('index.html')
+
+
+@app.route("/api/v1/csvToBvh",methods=["POST"])
+@cross_origin()
+def convert_csv_to_bvh():
+    
+    if 'file' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    print(file.filename)
+    if file.filename == '':
+        resp = jsonify({'message' : 'No file selected for uploading'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        resp = jsonify({'message' : 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+        resp.status_code = 400
+        return resp
+
+if __name__ == "__main__":
+  app.run(debug=True,threaded=True)
